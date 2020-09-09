867536900fda93c9e1bc3928db3c1cc5b42cc62c
serosc95 <oscardleon95@gmail.com>
2020-09-09 00:12:45 +0000

    Ten

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,9 +4,10 @@ def divisible_by_2(my_list=[]):
     if my_list[0] % 2 != 0:
         result = False
     new_list = [result]
-    for i in range(1, len(my_list)):
-        result = True
-        if my_list[i] % 2 != 0:
-            result = False
-        new_list.append(result)
+    if len(my_list) > 1:
+        for i in range(1, len(my_list)):
+            result = True
+            if my_list[i] % 2 != 0:
+                result = False
+            new_list.append(result)
     return (new_list)
