8cdb7da47fd1a8a1be0ff8a8741e515437479a50
serosc95 <oscardleon95@gmail.com>
2020-09-20 00:10:38 +0000

    Fizz Buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+for i in range(1, 101):
+    if i % 3 == 0:
+        print("Fizz", end="")
+    if i % 5 == 0:
+        print("Buzz", end="")
+    if i % 3 != 0 and i % 5 != 0:
+        print("{:d}" .format(i), end="")
+    if i != 100:
+        print(" ", end="")
+print()
