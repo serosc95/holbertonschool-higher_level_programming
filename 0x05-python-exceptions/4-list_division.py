cf29f66579997bad7cc17233dd4f01ec787e7fa3
serosc95 <oscardleon95@gmail.com>
2020-09-22 00:37:51 +0000

    four tasks

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,18 +10,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
         except IndexError:
-            print("out of range")       
+            print("out of range")
         new_list[i] = div
     return new_list
-
-my_l_1 = [10, 8, 4]
-my_l_2 = [2, 4, 4]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
-
-print("--")
-
-my_l_1 = [10, 8, 4, 4]
-my_l_2 = [2, 0, "H", 2, 7]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
