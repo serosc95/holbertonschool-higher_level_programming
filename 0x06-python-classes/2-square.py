15ca5c430f04a97ca973da07cf14b7ddd8411a40
serosc95 <oscardleon95@gmail.com>
2020-09-23 03:05:39 +0000

    tasks two

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""The class Square"""
+
+
+class Square:
+    """Class Square empty
+
+    Attributes:
+       __size: size
+    """
+    def __init__(self, size=0):
+        """__init__ method
+
+        Arguments:
+        self: self
+        size (int): size
+        Returns: No Value
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
