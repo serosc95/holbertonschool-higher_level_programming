3e4548a36a67eca4e93112819cc286d0d4c5dc30
serosc95 <oscardleon95@gmail.com>
2020-09-23 02:50:12 +0000

    tasks one

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+"""The class Square"""
+
+
 class Square:
+    """Class Square empty
+
+    Attributes:
+       __size: size
+    """
     def __init__(self, size):
+        """__init__ method
+
+        Arguments:
+        self: self
+        size (int): size
+        Returns: No Value
+        """
         self.__size = size
