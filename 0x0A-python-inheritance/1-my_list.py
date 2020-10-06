d7f463dcb7eeb081830365d47fdc6df69fffcf75
serosc95 <oscardleon95@gmail.com>
2020-10-06 23:04:47 +0000

    All

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+
+
 """ The class MyList"""
 
 
 class MyList(list):
-    """ Class MyList inherits from list
-    """
+    """ Class MyList inherits from list """
     def print_sorted(self):
         """ prints the list, but sorted """
         print(sorted(self))
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+
+
 def inherits_from(obj, a_class):
     return (isinstance(obj, a_class) and not (type(obj) == a_class))
