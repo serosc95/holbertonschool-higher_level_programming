29ee0342d2fbef096bfbd978f911ce73e1d50322
serosc95 <oscardleon95@gmail.com>
2020-10-07 23:49:01 +0000

    tasks eigth

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -8,4 +8,4 @@ import json
 def load_from_json_file(filename):
     """ Function load_from_json_file """
     with open(filename, 'r') as myFile:
-        return json.loads(myFile)
+        return json.load(myFile)
