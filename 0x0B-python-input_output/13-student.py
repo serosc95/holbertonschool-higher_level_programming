60f3d1beb096ffab1494031feaa1f004544c7d59
serosc95 <oscardleon95@gmail.com>
2020-10-08 02:50:36 +0000

    Tasks

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Module """
+
+
+class Student:
+    """ Class Student """
+    def __init__(self, first_name, last_name, age):
+        """ __init__ method
+        Arguments:
+            self: self
+            first_name: first_name
+            last_name: last_name
+            age: age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ class_to_json method
+        Arguments:
+            self: self
+        """
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new_dict[key] = value
+        return new_dict
+
+    def reload_from_json(self, json):
+        """ reload_from_json method
+        Arguments:
+            self: self
+            json: json
+        """
+        for key, value in json.items():
+            self.__dict__[key] = value
