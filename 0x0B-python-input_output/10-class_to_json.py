7d9a79240369bd3b4f5551924b405362edb82f11
serosc95 <oscardleon95@gmail.com>
2020-10-08 02:19:14 +0000

    Ten

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -2,23 +2,6 @@
 """ Module """
 
 
-class Student:
-    """ Class Student """
-    def __init__(self, first_name, last_name, age):
-        """ __init__ method
-        Arguments:
-            self: self
-            first_name: first_name
-            last_name: last_name
-            age: age
-        """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def class_to_json(obj):
-        """ class_to_json method
-        Arguments:
-            self: self
-        """
-        return self.__dict__
+def class_to_json(obj):
+    """ Function class_to_json """
+    return obj.__dict__
