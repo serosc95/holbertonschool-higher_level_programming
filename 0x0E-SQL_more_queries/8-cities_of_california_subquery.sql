c92af7a5bed862066e4e1c7a40f9531ebc307da1
serosc95 <oscardleon95@gmail.com>
2020-11-18 02:47:13 +0000

    subquieries

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
 
-SELECT * FROM hbtn_0d_usa.cities WHERE state_id = (SELECT id FROM hbtn_0d_usa.states WHERE name = "California");
+SELECT id, name FROM hbtn_0d_usa.cities WHERE state_id = (SELECT id FROM hbtn_0d_usa.states WHERE name = "California");
