30fb12cf8f48b5822d51b88d5509c3272fbbbed3
serosc95 <oscardleon95@gmail.com>
2020-11-18 16:08:43 +0000

    join = inner join

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- Import the database dump from hbtn_0d_tvshows
+
+SELECT name 
+	FROM tv_genres
+	JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+	JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_shows.title = "Dexter"
+
