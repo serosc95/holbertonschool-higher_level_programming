b841dbce63e706b5d2f789a1fa93b4b726cc7c63
serosc95 <oscardleon95@gmail.com>
2020-11-18 16:23:31 +0000

    the same

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- Import the database dump from hbtn_0d_tvshows
+
+SELECT title
+        FROM tv_shows
+	INNER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+	INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+        WHERE tv_genres.name = "Comedy"
+	ORDER BY title
