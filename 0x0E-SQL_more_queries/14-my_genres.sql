0c848002b7fdad4272da2ee9ebce2cfe2cf66a0b
serosc95 <oscardleon95@gmail.com>
2020-11-18 16:16:24 +0000

    join = inner join

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,4 +5,4 @@ SELECT name
 	JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 	JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 	WHERE tv_shows.title = "Dexter"
-
+	ORDER BY name
