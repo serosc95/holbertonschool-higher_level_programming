eed8a118017b6b29caa2d6aaadd2eed370549426
serosc95 <oscardleon95@gmail.com>
2020-11-18 17:29:03 +0000

    sibqueries and join

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -2,8 +2,8 @@
 
 SELECT name FROM tv_genres
 WHERE name NOT IN (
-	SELECT name FROM tv_genres 
-	INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-        INNER JOIN tv_genres ON tv_show_genres.show_id = tv_shows.id
-        WHERE tv_shows.title = "Dexter")
+	SELECT name FROM tv_genres
+	JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+	JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_shows.title = "Dexter")
 ORDER BY name
