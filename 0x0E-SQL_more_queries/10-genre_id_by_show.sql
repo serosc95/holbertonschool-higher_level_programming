514ffc951dbd186c54736e57f97d8864a6bc0946
serosc95 <oscardleon95@gmail.com>
2020-11-18 03:22:02 +0000

    inner Join

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,3 @@
+-- Import the database dump from hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show INNER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id IS NOT NULL
+	GROUP BY tv_shows.title, tv_show_genres.genre_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
