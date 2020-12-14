18e1ea1452bd5f0dc41e41b075ed244af5ac44aa
serosc95 <oscardleon95@gmail.com>
2020-12-14 03:04:16 +0000

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
-    condition = argv[4]
     conn = MySQLdb.connect(host="localhost", port=3306, user=user,
                            passwd=passwd, db=db, charset="utf8")
     cur = conn.cursor()
