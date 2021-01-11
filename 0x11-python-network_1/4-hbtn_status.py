bccf6fe802ad9de8677b7f99892756baf2803bcf
serosc95 <oscardleon95@gmail.com>
2021-01-11 22:46:16 +0000

    module requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status"""
+import requests
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    req = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}" .format(type(req.text)))
+    print("\t- content: {}" .format(req.text))
