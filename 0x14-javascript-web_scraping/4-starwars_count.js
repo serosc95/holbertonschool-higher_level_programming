7c92104dd702838db7ade2f7237241807c2f9c8b
serosc95 <oscardleon95@gmail.com>
2021-02-10 01:31:04 +0000

    request get characteres and forloop

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,7 +5,9 @@ const character = 'https://swapi-api.hbtn.io/api/people/18/';
 request(process.argv[2], function (error, response) {
   if (!error) {
     for (const film of JSON.parse(response.body).results) {
-      count = (film.characters.indexOf(character) >= 0) ? count + 1 : count;
+      for (const figures of film.characters) {
+        count = (figures === character) ? count + 1 : count;
+      }
     }
     console.log(count);
   }
