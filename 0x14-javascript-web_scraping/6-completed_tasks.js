6b5465b1b7b17be7fb4da4ebd3e399e2b4b2e010
serosc95 <oscardleon95@gmail.com>
2021-02-10 02:30:08 +0000

    computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
 const obj = {};
-request(process.argv[2], function (error, response) {
+const url = `${process.argv[2]}?completed=true`;
+request(url, function (error, response) {
   if (!error) {
     for (const index of JSON.parse(response.body)) {
-      if (index.completed === true) {
-        obj[index.userId] = (!obj[index.userId]) ? 1 : obj[index.userId] + 1;
-      }
+      obj[index.userId] = (!obj[index.userId]) ? 1 : obj[index.userId] + 1;
     }
   }
   console.log(obj);
